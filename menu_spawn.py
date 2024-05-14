d728d88c736dc6a49b0803ef71c3a31a59bd4817
collinBlessing <nahuriracollinblessing@gmail.com>
2024-05-14 18:38:31 +0300

    modifying changes and settings working well

--- menu_spawn.py
+++ menu_spawn.py
@@ -3,68 +3,86 @@ from PIL import Image, ImageTk
 import os
 from tkinter.filedialog import askdirectory
 import webbrowser
+import sqlite3 as sq
 
 
-CURRENT_THEME = 'red'
-NEW_THEME = ''
+conn = sq.connect("info.db")
+exc = conn.cursor()
 
 
 def Preferences():
+    def saveChanges():
+        CURRENT_THEME = exc.execute("SELECT current_theme from theme").fetchone()[0]
+
+        NEW_THEME = themeOptions.get()
+
+        if NEW_THEME != CURRENT_THEME:
+            exc.execute("UPDATE theme SET current_theme = ? WHERE ID = 1", (NEW_THEME,))
+            conn.commit()
+
+            text.configure(
+                text="Changes saved successfully", bg_color="green", text_color="white"
+            )
+            text.place(x=0, y=0)
+        else:
+            text.configure(
+                text="No changes to make", bg_color="yellow", text_color="black"
+            )
+            text.place(x=0, y=0)
+
     def chooseSelection():
-        new_path = '{}'.format(askdirectory(
-            title='Choose directrory', mustexist=True))
+        new_path = "{}".format(askdirectory(title="Choose directrory", mustexist=True))
         print(new_path)
 
     preferences_window = ctk.CTk()
     preferences_window.geometry("600x320")
     preferences_window.title("Preferences")
     iconpath = ImageTk.PhotoImage(file=os.path.join("assets", "logo.png"))
-    preferences_window.after(
-        300, lambda: preferences_window.iconphoto(False, iconpath))
+    preferences_window.after(300, lambda: preferences_window.iconphoto(False, iconpath))
     preferences_window.resizable(0, 0)
-
-
 
     # unsaved changes
     text = ctk.CTkLabel(
-        preferences_window, text="unsaved changes", text_color="black", width=600, bg_color="yellow"
+        preferences_window,
+        text_color="white",
+        width=600,
+        bg_color="green",
     )
-    text.place(x=0, y=0)
-
-
 
     downloadFolder = ctk.CTkFrame(preferences_window)
     # Labels
-    download_folder_label = ctk.CTkLabel(
-        downloadFolder, text="Download Folder:")
-    download_folder_label.pack(side='left', padx=10, pady=10)
+    download_folder_label = ctk.CTkLabel(downloadFolder, text="Download Folder:")
+    download_folder_label.pack(side="left", padx=10, pady=10)
     # Labels
     download_folder_location = ctk.CTkLabel(
-        downloadFolder, text="/home/collyne/downloads", wraplength=150, text_color="green")
+        downloadFolder,
+        text="/home/collyne/downloads",
+        wraplength=150,
+        text_color="green",
+    )
 
-    download_folder_location.pack(side='left', padx=10, pady=10)
+    download_folder_location.pack(side="left", padx=10, pady=10)
 
     # Button
     download_folder_Button = ctk.CTkButton(
-        downloadFolder, text="change",  command=chooseSelection)
-    download_folder_Button.pack(side='left', padx=10, pady=10)
+        downloadFolder, text="change", command=chooseSelection
+    )
+    download_folder_Button.pack(side="left", padx=10, pady=10)
 
-    downloadFolder.pack(padx=10, pady=(50,10))
+    downloadFolder.pack(padx=10, pady=(50, 10))
 
     # theme text
     themeFrame = ctk.CTkFrame(preferences_window)
-    themeText = ctk.CTkLabel(
-        themeFrame, text="Choose Theme", wraplength=150)
+    themeText = ctk.CTkLabel(themeFrame, text="Choose Theme", wraplength=150)
 
-    themeText.pack(side = "left", padx=(10, 60), pady=10)
+    themeText.pack(side="left", padx=(10, 60), pady=10)
 
-# TODO: fix error on about page
+    # TODO: fix error on about page
     # options
 
-    themes = ['marsh', 'metal',
-              'pink', 'red', 'violet', 'yellow']
-    themeOptions = ctk.CTkOptionMenu(
-        themeFrame, values=themes)
+    themes = ["marsh", "metal", "pink", "red", "violet", "yellow"]
+    themeOptions = ctk.CTkOptionMenu(themeFrame, values=themes)
+    CURRENT_THEME = exc.execute("SELECT current_theme from theme").fetchone()[0]
     themeOptions.set(CURRENT_THEME)
 
     themeOptions.pack(side="left", padx=10, pady=10)
@@ -72,12 +90,12 @@ def Preferences():
 
     # Automatically download files
     automatic_download = ctk.CTkCheckBox(
-        preferences_window, text='start downloading files automatically')
+        preferences_window, text="start downloading files automatically"
+    )
     automatic_download.pack(padx=(10, 60), pady=10)
 
-
     # save button
-    saveButton = ctk.CTkButton(preferences_window, text='Save')
+    saveButton = ctk.CTkButton(preferences_window, text="Save", command=saveChanges)
     saveButton.pack(padx=10, pady=10)
 
     preferences_window.mainloop()
@@ -104,10 +122,11 @@ def About():
 
     link_text = "http://www.nahuriracollinblessing.com"
     button_link = ctk.CTkButton(
-        about_window, height=5, text='Visit portfolio', command=visitPortfolio)
+        about_window, height=5, text="Visit portfolio", command=visitPortfolio
+    )
     button_link.pack(padx=10, pady=10)
 
     about_window.mainloop()
 
 
-# Preferences()
+Preferences()
